4595842a1d92da550d187b51c7abdf246b967880
owajbhai <owajshaikhrcm@gmail.com>
2025-06-01 21:33:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ class Config(object):
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "botmaster55")
       ADMIN_ID = int(os.environ.get("ADMIN_ID", 1249672673 )) 
-      # DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mongodb011:rxXV4pGzxLJgxaXQ@cluster0.undjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+      # DB_URL = os.environ.get("DATABASE_URL", "")
