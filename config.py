70fba4a7c2e5533146deddbc4590d78adbee97b5
owajbhai <owajshaikhrcm@gmail.com>
2025-06-01 20:45:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import os
 
 class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7084999554:AAGpuliYzeu-jDcigEs7O5xHJwHWLqiekkQ")
-      API_ID = int(os.environ.get("APP_ID", 21179966))
-      API_HASH = os.environ.get("API_HASH", 'd97919fb0a3c725e8bb2a25bbb37d57c')
+      API_ID = int(os.environ.get("APP_ID", 23621595))
+      API_HASH = os.environ.get("API_HASH", 'de904be2b4cd4efe2ea728ded17ca77d')
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Scammer_botxz")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 7326397503 )) 
-      # DB_URL = os.environ.get("DATABASE_URL", "")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "botmaster55")
+      ADMIN_ID = int(os.environ.get("ADMIN_ID", 1249672673 )) 
+      # DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://mongodb011:rxXV4pGzxLJgxaXQ@cluster0.undjh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
